9dae7529809178dc9d3d87f7191d41401155c1b9
Andriy-Lesyk <74345435+Andriy-Lesyk@users.noreply.github.com>
2021-05-08 14:31:32 +0300

    Решает задачу

---
+++ index.js
@@ -0,0 +1,35 @@
+const users = [
+    { name: 'Mango', active: true },
+    { name: 'Poly', active: false },
+    { name: 'Ajax', active: true },
+    { name: 'Lux', active: false },
+  ];
+
+  /*const toggleUserState = (allUsers, userName, callback) => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
+  
+    callback(updatedUsers);
+  };
+  const logger = updatedUsers => console.table(updatedUsers);
+  
+    toggleUserState(users, 'Mango', logger);
+    toggleUserState(users, 'Ajax', logger);*/
+
+const toggleUserState= (allUsers, userName)=>{    
+    return new Promise((resolve, reject)=>{
+        const updatedUsers =allUsers.map(user =>
+                user.name === userName ? 
+                resolve( { ...user, active: !user.active }) : 
+                reject(user));               
+    });   
+}
+    
+    const logger = updatedUsers => console.table(updatedUsers);
+
+    toggleUserState(users, 'Mango').then(logger);
+    toggleUserState(users, 'Lux').then(logger);
+    
+    
+  
